13c0cedff44800da8f637560612008e9fb79511e
Mihail <67411432+mihailvasilev77@users.noreply.github.com>
2023-07-13 10:24:01 +0300

    Added a filter option by year.

--- app.py
+++ app.py
@@ -6,16 +6,20 @@ app = Flask(__name__, template_folder="templates")
 
 @app.route('/', methods=['GET', 'POST'])
 def dashboard():
+    df = pd.read_csv('raw_data.csv')
+
+    dropdown_options = df['year'].unique()
+
     if request.method == 'POST':
-        selected_columns = request.form.getlist('columns')
+        selected_value = request.form.get('dropdown')
 
-        df = pd.read_csv('raw_data.csv')
+        if selected_value and selected_value != 'All':
+            df = df[df['year'].astype(str) == selected_value]
 
-        fig = px.bar(df, x = 'ACCOUNT_START_DATE', y = 'CNT_SUB')
 
-        return render_template('index.html', plot=fig.to_html(full_html=False), columns=df.columns)
+    fig = px.bar(df, x = 'ACCOUNT_START_DATE', y = 'CNT_SUB')
 
-    return render_template('index.html', columns=[])
+    return render_template('index.html', plot=fig.to_html(full_html=False), dropdown_options=dropdown_options)
 
 if __name__ == '__main__':
     app.run(debug=True)
