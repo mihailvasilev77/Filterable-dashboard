a9d93bc24919da38142928563b99fc399a5223c1
Mihail <67411432+mihailvasilev77@users.noreply.github.com>
2023-07-13 11:18:31 +0300

    Dynamic selection on the filter

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, request
 import pandas as pd
 import plotly.express as px
+import json
 
 app = Flask(__name__, template_folder="templates")
 
@@ -8,18 +9,23 @@ app = Flask(__name__, template_folder="templates")
 def dashboard():
     df = pd.read_csv('raw_data.csv')
 
-    dropdown_options = df['year'].unique()
+
+    column_options = df.columns.tolist()
 
     if request.method == 'POST':
-        selected_value = request.form.get('dropdown')
+        selected_column = request.form.get('column')  # Get the selected column from the dropdown
+        selected_value = request.form.get('value')  # Get the value entered by the user
 
-        if selected_value and selected_value != 'All':
-            df = df[df['year'].astype(str) == selected_value]
+        if selected_column and selected_value:
+            df = df[df[selected_column].astype(str) == selected_value]
 
 
     fig = px.bar(df, x = 'ACCOUNT_START_DATE', y = 'CNT_SUB')
 
-    return render_template('index.html', plot=fig.to_html(full_html=False), dropdown_options=dropdown_options)
+    unique_values = {column: df[column].unique().tolist() for column in column_options}
+    unique_values_json = json.dumps(unique_values)
+
+    return render_template('index.html', plot=fig.to_html(full_html=False), column_options=column_options, unique_values=unique_values_json)
 
 if __name__ == '__main__':
     app.run(debug=True)
