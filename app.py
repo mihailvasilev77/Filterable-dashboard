f405aa3e15ffa5bae538dd14c6fcbdc0ab1f702a
Mihail <67411432+mihailvasilev77@users.noreply.github.com>
2023-07-14 10:23:00 +0300

    Added more filters
    Improved to use multiple filters working simultaneously, removing the old option to filter based on each category, but saving the first design in its own file.

--- app.py
+++ app.py
@@ -9,23 +9,79 @@ app = Flask(__name__, template_folder="templates")
 def dashboard():
     df = pd.read_csv('raw_data.csv')
 
-
-    column_options = df.columns.tolist()
-
     if request.method == 'POST':
-        selected_column = request.form.get('column')  # Get the selected column from the dropdown
-        selected_value = request.form.get('value')  # Get the value entered by the user
+        selected_segment = request.form.get('segment')
+        selected_price_plan = request.form.get('price_plan')
+        selected_discounted_mf = request.form.get('discounted_mf')
+        selected_admin_center = request.form.get('admin_center')
 
-        if selected_column and selected_value:
-            df = df[df[selected_column].astype(str) == selected_value]
+        filtered_df = df.copy()
 
+        if selected_segment and selected_segment != 'All':
+            filtered_df = filtered_df[filtered_df['SEGMENT_NAME'] == selected_segment]
+        if selected_price_plan and selected_price_plan != 'All':
+            filtered_df = filtered_df[filtered_df['PRICE_PLAN_DESC'] == selected_price_plan]
+        if selected_discounted_mf and selected_discounted_mf != 'All':
+            filtered_df = filtered_df[filtered_df['DISCOUNTED_MF_W_VAT'] == selected_discounted_mf]
+        if selected_admin_center and selected_admin_center != 'All':
+            filtered_df = filtered_df[filtered_df['ADMIN_CENTER'] == selected_admin_center]
 
-    fig = px.bar(df, x = 'ACCOUNT_START_DATE', y = 'CNT_SUB')
+        fig = px.bar(filtered_df, x='ACCOUNT_START_DATE', y='CNT_SUB')
 
-    unique_values = {column: df[column].unique().tolist() for column in column_options}
-    unique_values_json = json.dumps(unique_values)
+    else:
+        fig = px.bar(df, x='ACCOUNT_START_DATE', y='CNT_SUB')
 
-    return render_template('index.html', plot=fig.to_html(full_html=False), column_options=column_options, unique_values=unique_values_json)
+    segment_options = df['SEGMENT_NAME'].unique().tolist()
+    price_plan_options = df['PRICE_PLAN_DESC'].unique().tolist()
+    discounted_mf_options = df['DISCOUNTED_MF_W_VAT'].unique().tolist()
+    admin_center_options = df['ADMIN_CENTER'].unique().tolist()
+
+    return render_template('index.html', plot=fig.to_html(full_html=False), segment_options=segment_options, price_plan_options=price_plan_options, discounted_mf_options=discounted_mf_options, admin_center_options=admin_center_options)
+
+@app.route('/get_price_plans', methods=['POST'])
+def get_price_plans():
+    segment = request.form.get('segment')
+
+    df = pd.read_csv('raw_data.csv')
+    if segment and segment != 'All':
+        df = df[df['SEGMENT_NAME'] == segment]
+
+    price_plan_options = df['PRICE_PLAN_DESC'].unique().tolist()
+
+    return json.dumps(price_plan_options)
+
+@app.route('/get_discounted_mf', methods=['POST'])
+def get_discounted_mf():
+    segment = request.form.get('segment')
+    price_plan = request.form.get('price_plan')
+
+    df = pd.read_csv('raw_data.csv')
+    if segment and segment != 'All':
+        df = df[df['SEGMENT_NAME'] == segment]
+    if price_plan and price_plan != 'All':
+        df = df[df['PRICE_PLAN_DESC'] == price_plan]
+
+    discounted_mf_options = df['DISCOUNTED_MF_W_VAT'].unique().tolist()
+
+    return json.dumps(discounted_mf_options)
+
+@app.route('/get_admin_centers', methods=['POST'])
+def get_admin_centers():
+    segment = request.form.get('segment')
+    price_plan = request.form.get('price_plan')
+    discounted_mf = request.form.get('discounted_mf')
+
+    df = pd.read_csv('raw_data.csv')
+    if segment and segment != 'All':
+        df = df[df['SEGMENT_NAME'] == segment]
+    if price_plan and price_plan != 'All':
+        df = df[df['PRICE_PLAN_DESC'] == price_plan]
+    if discounted_mf and discounted_mf != 'All':
+        df = df[df['DISCOUNTED_MF_W_VAT'] == discounted_mf]
+
+    admin_center_options = df['ADMIN_CENTER'].unique().tolist()
+
+    return json.dumps(admin_center_options)
 
 if __name__ == '__main__':
     app.run(debug=True)
---
+++ static/script.js
@@ -0,0 +1,55 @@
+$(document).ready(function() {
+    $('#segment-dropdown').change(function() {
+        var selectedSegment = $(this).val();
+        $.ajax({
+            url: '/get_price_plans',
+            type: 'POST',
+            data: {segment: selectedSegment},
+            success: function(response) {
+                var pricePlanDropdown = $('#price-plan-dropdown');
+                pricePlanDropdown.empty();
+                pricePlanDropdown.append($('<option>').text('All').attr('value', 'All'));
+                $.each(JSON.parse(response), function(index, option) {
+                    pricePlanDropdown.append($('<option>').text(option).attr('value', option));
+                });
+            }
+        });
+    });
+
+    $('#price-plan-dropdown').change(function() {
+        var selectedSegment = $('#segment-dropdown').val();
+        var selectedPricePlan = $(this).val();
+        $.ajax({
+            url: '/get_discounted_mf',
+            type: 'POST',
+            data: {segment: selectedSegment, price_plan: selectedPricePlan},
+            success: function(response) {
+                var discountedMFDropdown = $('#discounted-mf-dropdown');
+                discountedMFDropdown.empty();
+                discountedMFDropdown.append($('<option>').text('All').attr('value', 'All'));
+                $.each(JSON.parse(response), function(index, option) {
+                    discountedMFDropdown.append($('<option>').text(option).attr('value', option));
+                });
+            }
+        });
+    });
+
+    $('#discounted-mf-dropdown').change(function() {
+        var selectedSegment = $('#segment-dropdown').val();
+        var selectedPricePlan = $('#price-plan-dropdown').val();
+        var selectedDiscountedMF = $(this).val();
+        $.ajax({
+            url: '/get_admin_centers',
+            type: 'POST',
+            data: {segment: selectedSegment, price_plan: selectedPricePlan, discounted_mf: selectedDiscountedMF},
+            success: function(response) {
+                var adminCenterDropdown = $('#admin-center-dropdown');
+                adminCenterDropdown.empty();
+                adminCenterDropdown.append($('<option>').text('All').attr('value', 'All'));
+                $.each(JSON.parse(response), function(index, option) {
+                    adminCenterDropdown.append($('<option>').text(option).attr('value', option));
+                });
+            }
+        });
+    });
+});
